5cc73061b4e1d644396edc8358dcf062cf777e3d
seven <seven@80238c21-1b17-0410-8270-c26cd0aec9e0>
2010-08-27 10:00:58 +0000

    add resources
    git-svn-id: http://svn.idapted.com/svn/projects/voips/trainer_studio/trunk@27804 80238c21-1b17-0410-8270-c26cd0aec9e0

---
+++ resources/loadflash.js
@@ -0,0 +1,10 @@
+
+var t = '<embed src="http://www.eqenglish.com/flex/interaction/trainer/interaction.swf" ';
+t += 'quality="high" bgcolor="" wmode="opaque" ';
+t += 'width="100%" height="100%" name="interaction" align="middle" ';
+t += 'play="true" loop="false"  allowScriptAccess="sameDomain" type="application/x-shockwave-flash" ';
+t += 'pluginspage="http://www.adobe.com/go/getflashplayer" ';
+t += 'flashvars = "' + vars + '"></embed>';
+
+//alert(vars);
+document.write(t);
