84349368883f82ad156b7c6cdab322eefe3cb908
sonic <sonic@80238c21-1b17-0410-8270-c26cd0aec9e0>
2010-09-16 03:42:30 +0000

    git-svn-id: http://svn.idapted.com/svn/projects/voips/trainer_studio/trunk@28286 80238c21-1b17-0410-8270-c26cd0aec9e0

--- libs/qjson-0.7.1/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Qt/qjson-0.7.1/qjson
-# Build directory: D:/Qt/qjson-0.7.1/qjson/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(tests)
-SUBDIRS(cmake/modules)
--- libs/qjson-0.7.1/build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Qt/qjson-0.7.1/qjson/tests
-# Build directory: D:/Qt/qjson-0.7.1/qjson/build/tests
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(testparser "testparser")
-ADD_TEST(testqobjecthelper "testqobjecthelper")
-ADD_TEST(testserializer "testserializer")
