e71b9b11c23a719b9294672257a8a4d0dc6343e9
Jacob Lewallen <jlewallen@gmail.com>
2010-11-01 14:15:15 -0700

    Moved common logging code around. Pass onto old handler.
    Passing onto the old handler makes for easier work under
    windows.. it'll show the log messages in the output pane
    in the IDE so we can avoid checking the log file.

--- main.cpp
+++ main.cpp
@@ -4,25 +4,54 @@
 #include "main_window.h"
 #include "fs_host.h"
 
-FILE *logFile;
+FILE *_logFile = NULL;
+QtMsgHandler oldMsgHandler = NULL;
 
-void myMessageHandler(QtMsgType type, const char *msg)
+void customMessageHandler(QtMsgType type, const char *msg)
 {
   switch (type) {
   case QtDebugMsg:
-    fprintf(logFile, "Debug: %s\n", msg);
+    fprintf(_logFile, "Debug: %s\n", msg);
     break;
   case QtWarningMsg:
-    fprintf(logFile, "Warning: %s\n", msg);
+    fprintf(_logFile, "Warning: %s\n", msg);
     break;
   case QtCriticalMsg:
-    fprintf(logFile, "Critical: %s\n", msg);
+    fprintf(_logFile, "Critical: %s\n", msg);
     break;
   case QtFatalMsg:
-    fprintf(logFile, "Fatal: %s\n", msg);
+    fprintf(_logFile, "Fatal: %s\n", msg);
     abort();
   }
-  fflush(logFile);
+  fflush(_logFile);
+  if (oldMsgHandler != NULL) {
+    oldMsgHandler(type, msg);
+  }
+}
+
+void configureLogging()
+{
+  QDir home = QDir::home();
+
+  if (!home.exists(DOTDIR "/log")) {
+    if (!home.mkpath(DOTDIR "/log")) {
+      fprintf(stderr, "Error opening log dir %s\n", home.absolutePath() + DOTDIR "/log");
+    }
+  }
+  QFile log(home.absoluteFilePath(DOTDIR "/log/trainer_studio.log"));
+
+  if (!(_logFile = fopen(log.fileName().toAscii(), "a+"))) {
+    fprintf(stderr, "Error opening log file %s\n", log.fileName().toAscii());
+  }
+  else {
+    oldMsgHandler = qInstallMsgHandler(customMessageHandler);
+    fprintf(stderr, "HI");
+  }
+
+  QDateTime now = QDateTime::currentDateTime();
+  qDebug() << "\n\n============================================\n" <<
+            "TS starting at: " << now.toString("yyyy-MM-dd hh:mm:ss")  <<
+          "\n============================================\n";
 }
 
 bool setDefaultSettings()
@@ -38,35 +67,13 @@ bool setDefaultSettings()
 
 int main(int argc, char *argv[])
 {
-
-  //    QtSingleApplication a(argc, argv);
   QApplication a(argc, argv);
   QApplication::setApplicationName("Trainer Studio");
   QApplication::setApplicationVersion("3.0.1");
   QApplication::setOrganizationName("Idapted Ltd.");
   QApplication::setOrganizationDomain("idapted.com");
 
-  QDir home = QDir::home();
-
-  if (!home.exists(DOTDIR "/log")) {
-    if (!home.mkpath(DOTDIR "/log")) {
-      fprintf(stderr, "Error opening log dir %s\n", home.absolutePath() + DOTDIR "/log");
-    }
-  }
-  QFile log(home.absoluteFilePath(DOTDIR "/log/trainer_studio.log"));
-
-  if (!(logFile = fopen(log.fileName().toAscii(), "a+"))) {
-    fprintf(stderr, "Error opening log file %s\n", log.fileName().toAscii());
-  }
-  else {
-    qInstallMsgHandler(myMessageHandler);
-  }
-
-  QDateTime now = QDateTime::currentDateTime();
-  qDebug() << "\n\n============================================\n"
-   "TS starting at:"
-   << now.toString("yyyy-MM-dd hh:mm:ss")
-   << "\n============================================\n";
+  configureLogging();
 
   fshost = new FSHost();
   fshost->start();
@@ -83,12 +90,7 @@ int main(int argc, char *argv[])
 
   MainWindow w;
   w.showLoginDialog();
-  //We have to show the mainwindow as well,
-  //Because there are no icon on taskbar if not
   w.show();
-
   return a.exec();
-
-  if (logFile) fclose(logFile);
 }
 
