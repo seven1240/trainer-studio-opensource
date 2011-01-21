28bb238f628174e0b16f2706ae6f9f8a7e60b869
Seven Du <dujinfang@gmail.com>
2011-01-21 15:41:48 +0800

    hope this fixes the weired input problem

--- resources/loadflash.js
+++ resources/loadflash.js
@@ -10,25 +10,12 @@ function idp_xx_movie_DoFSCommand(cmd, args) {
     }
 }
 
-// If the movie already there
-//var obj;
-//if(obj = document.getElementById("idp_xx_movie") ) {
-//    //found
-//    mainWindow.onFSCommand("log", "FlashVars: " + vars);
-//    mainWindow.onFSCommand("log", "Url: " + url);
-//    obj.Stop();
-//    obj.FlashVars = vars;
-//    obj.LoadMovie(0, url);
-//    return;
-//}
 
 // Create movie
 var t = '';
 t += '<embed name="idp_xx_movie" id="idp_xx_movie" src="' + url + '" ';
-t += 'quality="high" bgcolor="" wmode="opaque" ';
-t += 'width="100%" height="100%" name="interaction" align="middle" ';
-t += 'play="true" loop="false"  allowScriptAccess="sameDomain" type="application/x-shockwave-flash" ';
-t += 'pluginspage="http://www.adobe.com/go/getflashplayer" ';
+t += 'width="100%" height="100%" ';
+t += 'type="application/x-shockwave-flash" ';
 t += 'flashvars="' + vars + '"></embed>';
 
 document.close();
